92358f4fd6891b15cfdc320a94e7d20236ae377b
hc2014 <hc215324>
2018-08-10 10:40:41 +0800

    再测试一下

--- Rabbit/Send/Program.cs
+++ Rabbit/Send/Program.cs
@@ -10,7 +10,7 @@ namespace Send
     {
         static void Main(string[] args)
         {
-            //测试一下git
+            //测试一下git22
             var factory = new ConnectionFactory() { HostName = "localhost", UserName = "hc", Password="123456" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
